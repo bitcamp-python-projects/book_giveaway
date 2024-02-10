8fd6cefbf2451e2fc1f61e735736e3319e842e44
marie1307 <marikalmakhelidze@gmail.com>
2024-02-11 00:31:29 +0400

    add models

---
+++ books/serializers.py
@@ -0,0 +1,38 @@
+from .models import Book, WishList, Author, Genre, Condition
+from rest_framework import serializers
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = fields = "__all__"
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = fields = "__all__"
+
+
+class ConditionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Condition
+        fields = fields = "__all__"
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['title']
+
+
+class BookDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = "__all__"
+
+
+class WishListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WishList
+        fields = "__all__"
