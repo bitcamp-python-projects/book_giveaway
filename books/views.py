a6424bac078f671b9996a97e25e0f57843c3c861
sandrik-10 <sandrotsulaia@gmail.com>
2024-02-12 15:34:46 +0400

    add filters functionality  and search by book's title

--- books/views.py
+++ books/views.py
@@ -1,14 +1,17 @@
 from rest_framework import viewsets, permissions
 from .models import Book, WishList
 from .serializers import BookSerializer, BookDetailSerializer, WishListSerializer
-
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 # წიგნების სახელწოდებები
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['author', 'genre', 'condition', 'owner']
+    search_fields = ['title']
 # წიგნების შესახებ სრული ინფორმაცია
 class BookDetailViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
@@ -20,3 +23,4 @@ class WishListViewSet(viewsets.ModelViewSet):
     queryset = WishList.objects.all()
     serializer_class = WishListSerializer
     # permission_classes = [permissions.IsAuthenticated]
+  
